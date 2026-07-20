469e216c0b780a6364e420fa2236d3ab3f3632e8
amin <mohammadaminfanaie@gmail.com>
2026-07-21 02:25:57 +0330

    adding .example files

---
+++ app/prompts.example.py
@@ -0,0 +1,11 @@
+from langchain_core.prompts import PromptTemplate
+
+QUERY_ENHANCEMENT_PROMPT = PromptTemplate.from_template(''' ENTER YOUR SYSTEM PROMPT HERE''')
+
+
+ANSWER_GENERATION_PROMPT = PromptTemplate.from_template(""" ENTER YOUR SYSTEM PROMPT HERE""")
+
+
+# ======================================================================================================
+
+test_message= '''this is a test message :'''
